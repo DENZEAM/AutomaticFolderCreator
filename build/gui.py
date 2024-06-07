e40a77c31a4484346f2529475d7f3371d64fc726
DEN <68790210+DENZEAM@users.noreply.github.com>
2024-06-07 09:47:31 +0200

    chore: update GUI code formatting and imports

--- build/gui.py
+++ build/gui.py
@@ -1,15 +1,13 @@
-
-
 from pathlib import Path
 import AutomaticFolderCreator
 
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage , filedialog , messagebox
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, filedialog, messagebox
 import json
 import os
 from pathlib import Path
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\eljai\Documents\GitHub\AutomaticFolderCreator\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / "assets" / "frame0"
 
 
 def relative_to_assets(path: str) -> Path:
@@ -19,18 +17,22 @@ def relative_to_assets(path: str) -> Path:
 filePath = None
 Config = None
 
+
 def import_json():
     global Config
     filepath = filedialog.askopenfilename(filetypes=[("Fichiers JSON", "*.json")])
     if filepath:
-        with open(filepath, 'r') as file:
+        with open(filepath, "r") as file:
             data = json.load(file)
             Config = filepath
             print(Config)
-            messagebox.showinfo("Config sélectionnée", f"Config sélectionnée : {filepath}")
+            messagebox.showinfo(
+                "Config sélectionnée", f"Config sélectionnée : {filepath}"
+            )
         return data
     else:
         return None
+
 
 def select_path():
     global filePath
@@ -42,44 +44,37 @@ def select_path():
     else:
         return None
 
+
 def Create():
     print("Okkk")
     if Config and filePath:
         AutomaticFolderCreator.create_structure(Config, filePath)
     else:
-        messagebox.showerror("Erreur", "Veuillez sélectionner une configuration et un chemin.")
+        messagebox.showerror(
+            "Erreur", "Veuillez sélectionner une configuration et un chemin."
+        )
+
+
 window = Tk()
 
 window.geometry("745x504")
-window.configure(bg = "#FFFFFF")
+window.configure(bg="#FFFFFF")
 
 
 canvas = Canvas(
     window,
-    bg = "#FFFFFF",
-    height = 504,
-    width = 745,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
+    bg="#FFFFFF",
+    height=504,
+    width=745,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge",
 )
 
-canvas.place(x = 0, y = 0)
-canvas.create_rectangle(
-    0.0,
-    79.0,
-    745.0,
-    504.0,
-    fill="#8E91B0",
-    outline="")
+canvas.place(x=0, y=0)
+canvas.create_rectangle(0.0, 79.0, 745.0, 504.0, fill="#8E91B0", outline="")
 
-canvas.create_rectangle(
-    0.0,
-    0.0,
-    745.0,
-    80.0,
-    fill="#444A80",
-    outline="")
+canvas.create_rectangle(0.0, 0.0, 745.0, 80.0, fill="#444A80", outline="")
 
 canvas.create_text(
     17.0,
@@ -88,25 +83,14 @@ canvas.create_text(
     text="AUTOMATIC FOLDER CREATOR",
     fill="#FFFFFF",
     font=("Inter", 20 * -1),
-    justify="center"
+    justify="center",
 )
 
 canvas.create_text(
-    647.0,
-    33.0,
-    anchor="nw",
-    text="V.0.0.1",
-    fill="#FFFFFF",
-    font=("Inter", 13 * -1)
+    647.0, 33.0, anchor="nw", text="V.0.0.1", fill="#FFFFFF", font=("Inter", 13 * -1)
 )
 
-canvas.create_rectangle(
-    21.0,
-    96.0,
-    229.0,
-    420.0,
-    fill="#D9D9D9",
-    outline="")
+canvas.create_rectangle(21.0, 96.0, 229.0, 420.0, fill="#D9D9D9", outline="")
 
 canvas.create_text(
     47.0,
@@ -117,20 +101,18 @@ canvas.create_text(
     font=("Inter", 12 * -1),
     justify="center",
     width=250.0,
-  
 )
 
-canvas.create_text(
-    55.0,
-    324.0,
-    anchor="nw",
-    text="Importé une configuration",
-    fill="#000000",
-    font=("Inter", 12 * -1),
-    justify="center",
-    width=250.0
-    
-)
+# canvas.create_text(
+#     55.0,
+#     324.0,
+#     anchor="nw",
+#     text="Importé une configuration",
+#     fill="#000000",
+#     font=("Inter", 12 * -1),
+#     justify="center",
+#     width=250.0,
+# )
 
 canvas.create_text(
     55.0,
@@ -143,77 +125,47 @@ canvas.create_text(
     width=250.0,
 )
 
-button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
+button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
 button_1 = Button(
     image=button_image_1,
     borderwidth=2,
     highlightthickness=0,
     command=lambda: import_json(),
-    relief="flat"
+    relief="flat",
 )
-button_1.place(
-    x=50.0,
-    y=152.0,
-    width=150.0,
-    height=31.0
-)
+button_1.place(x=50.0, y=152.0, width=150.0, height=31.0)
 
-button_image_2 = PhotoImage(
-    file=relative_to_assets("button_2.png"))
-button_2 = Button(
-    image=button_image_2,
-    borderwidth=2,
-    highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
-    relief="flat"
-)
-button_2.place(
-    x=50.0,
-    y=356.0,
-    width=150.0,
-    height=31.0
-)
+# button_image_2 = PhotoImage(file=relative_to_assets("button_2.png"))
+# button_2 = Button(
+#     image=button_image_2,
+#     borderwidth=2,
+#     highlightthickness=0,
+#     command=lambda: print("button_2 clicked"),
+#     relief="flat",
+# )
+# button_2.place(x=50.0, y=356.0, width=150.0, height=31.0)
 
-button_image_3 = PhotoImage(
-    file=relative_to_assets("button_3.png"))
+button_image_3 = PhotoImage(file=relative_to_assets("button_3.png"))
 button_3 = Button(
     image=button_image_3,
     borderwidth=2,
     highlightthickness=0,
     command=lambda: select_path(),
-    relief="flat"
+    relief="flat",
 )
-button_3.place(
-    x=50.0,
-    y=250.0,
-    width=150.0,
-    height=31.0
-)
+button_3.place(x=50.0, y=250.0, width=150.0, height=31.0)
 
-button_image_4 = PhotoImage(
-    file=relative_to_assets("button_4.png"))
+button_image_4 = PhotoImage(file=relative_to_assets("button_4.png"))
 button_4 = Button(
     image=button_image_4,
     borderwidth=3,
     highlightthickness=0,
     command=lambda: Create(),
-    relief="flat"
+    relief="flat",
 )
-button_4.place(
-    x=29.0,
-    y=430.0,
-    width=180.0,
-    height=46.0
-)
+button_4.place(x=29.0, y=430.0, width=180.0, height=46.0)
 
-canvas.create_rectangle(
-    528.0,
-    121.0,
-    738.0,
-    463.0,
-    fill="#000000",
-    outline="")
+canvas.create_rectangle(528.0, 121.0, 738.0, 463.0, fill="#000000", outline="")
 
 canvas.create_text(
     532.0,
@@ -221,7 +173,7 @@ canvas.create_text(
     anchor="nw",
     text="[AFC] Chargment du systeme en cours",
     fill="#FFFFFF",
-    font=("Inter", 11 * -1)
+    font=("Inter", 11 * -1),
 )
 
 canvas.create_text(
@@ -230,11 +182,7 @@ canvas.create_text(
     anchor="nw",
     text="[AFC] Systeme Chargé !",
     fill="#FFFFFF",
-    font=("Inter", 11 * -1)
+    font=("Inter", 11 * -1),
 )
 window.resizable(False, False)
 window.mainloop()
-
-
-
-
